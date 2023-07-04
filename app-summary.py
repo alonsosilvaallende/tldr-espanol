8cb2a89e7ecbcc6c054d0795a54cfc7be77687eb
SILVA Alonso <alonso.silva@gmail.com>
2023-07-04 17:32:22 +0200

    Add reading time

--- app-summary.py
+++ app-summary.py
@@ -1,7 +1,8 @@
-#from dotenv import load_dotenv
-#load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 import os
+import numpy as np
 import tiktoken
 import streamlit as st
 from langchain.document_loaders import ToMarkdownLoader
@@ -11,47 +12,51 @@ from langchain.callbacks import get_openai_callback
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.summarize import load_summarize_chain
 import langchain
+from langchain.chains import LLMChain
 from langchain.cache import InMemoryCache
 langchain.llm_cache = InMemoryCache()
 
 api_key = os.getenv("TOMARKDOWN_API_KEY")
-
 encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
-def num_tokens_from_string(string: str) -> int:
-    """Returns the number of tokens in a text string."""
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
-
 prompt_template = """Write a concise summary of the following:
 
 {text}
 
 Remember that the president of Chile is Gabriel Boric.
 CONCISE SUMMARY IN SPANISH:"""
-
 PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
 
 st.title("TLDR en Español")
+st.markdown("TLDR en Español es una herramienta que te permite resumir el texto de una página web")
 
-URL = st.text_input("Ingresa la página a resumir:", "")
+URL = st.text_input("Ingresa la dirección de la página web a resumir (https://...):", "")
+def my_reading_time(num_tokens):
+    num_words = 0.75*num_tokens # source: https://openai.com/pricing
+    reading_time = num_words/250 # source: https://biblioguias.unex.es/c.php?g=572102&p=3944889#:~:text=El%20lector%20promedio%20est%C3%A1%20entre,con%20una%20serie%20de%20ejercicios.
+    reading_time = np.ceil(reading_time)
+    return reading_time
+
 if URL:
     loader = ToMarkdownLoader(url=URL, api_key=api_key)
     docs = loader.load()
     documents = docs[0].page_content
-
-
-    num_tokens = num_tokens_from_string(documents)
-    if num_tokens < 3200:
+    num_tokens = len(encoding.encode(documents))
+    st.write(f":green[Tiempo de lectura del artículo original: {my_reading_time(num_tokens):.0f} minutos]")
+    if num_tokens < 3800:
         llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0) 
-    else:
+        chain = LLMChain(llm=llm, prompt=PROMPT)
+    elif num_tokens < 15800:
         llm = ChatOpenAI(model_name='gpt-3.5-turbo-16k', temperature=0)
-	
-    text_splitter = SpacyTextSplitter(chunk_size=15000)
-    texts = text_splitter.create_documents([documents])
-
-    chain = load_summarize_chain(llm, chain_type="map_reduce", map_prompt=PROMPT, combine_prompt=PROMPT)
+        chain = LLMChain(llm=llm, prompt=PROMPT)
+    elif num_tokens < 55000:
+        text_splitter = SpacyTextSplitter(chunk_size=15000)
+        documents = text_splitter.create_documents([documents])
+        chain = load_summarize_chain(llm, chain_type="map_reduce", map_prompt=PROMPT, combine_prompt=PROMPT)
+    else:
+        st.write(f"The text is too long to be processed at this time")
     with get_openai_callback() as cb:
-        result = chain.run(texts)
-        st.write(cb)
+        result = chain.run(documents)
+        st.write(f":green[Tiempo de lectura del resumen: {my_reading_time(cb.completion_tokens):.0f} minuto]")
+        st.write(f":green[Costo del resumen: {cb.total_cost+.01:.6f} dólares]") #2MarkDown charges 1 cent per webpage
 	
     st.markdown(result)
